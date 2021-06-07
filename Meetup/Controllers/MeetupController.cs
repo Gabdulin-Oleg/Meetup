073640402874ae808ae7f45dd0c9dd5c5b252ae6
Oleja <gabdulin12@gmail.com>
2021-06-07 10:30:56 +0500

    added the ability to confirm Email

--- Meetup/Controllers/MeetupController.cs
+++ Meetup/Controllers/MeetupController.cs
@@ -1,6 +1,10 @@
-﻿using Meetup.Services.Interfaces;
+﻿using AutoMapper;
+using Meetup.ApplicationDbContext;
+using Meetup.ApplicationDbContext.Model;
+using Meetup.Services.Interfaces;
 using Meetup.ViewModels;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
 
@@ -10,13 +14,25 @@ namespace Meetup.Controllers
     [Route("[controller]")]
     public class MeetupController : ControllerBase
     {
-        IAdminService adminService;
-        IUserService userService;
+        private readonly UserManager<ApplicationUser> userManager;
+        private readonly SignInManager<ApplicationUser> signInManager;
+        IEmailSender emailService;
+        readonly AppDbContext dbContext;
+        IMapper mapper;
 
-        public MeetupController(IUserService userService, IAdminService adminService)
+
+        public MeetupController(SignInManager<ApplicationUser> signInManager, UserManager<ApplicationUser> userManager, AppDbContext dbContext, IEmailSender emailService)
         {
-            this.userService = userService;
-            this.adminService = adminService;
+            this.signInManager = signInManager;
+            this.userManager = userManager;
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.CreateMap<User, RegistredViewModel>();
+                cfg.CreateMap<RegistredViewModel, User>();
+            });
+            this.mapper = new Mapper(config);
+            this.dbContext = dbContext;
+            this.emailService = emailService;
         }
 
         [HttpGet]
@@ -24,44 +40,90 @@ namespace Meetup.Controllers
         {
             return Ok();
         }
+
         [HttpPost]
         public async Task<IActionResult> Register(RegistredViewModel model)
         {
-            bool result = await userService.RegistrationUserAsync(model);
+            var user = mapper.Map<User>(model);
+            ApplicationUser applicationUser = new ApplicationUser { Email = user.Email, UserName = user.Email };
+           var result = await userManager.CreateAsync(applicationUser, user.Password);
 
-            if (result)
+            if (result.Succeeded)
             {
+                var code = await userManager.GenerateEmailConfirmationTokenAsync(applicationUser);
+                var callbackUrl = Url.Action(
+                    "ConfirmEmail",
+                    "Meetup",
+                  new { userId = applicationUser.Id, code = code }, HttpContext.Request.Scheme,host: HttpContext.Request.Host.Value);//, host: "192.168.4.154:5001") ;
+                await emailService.SendEmailAsync(model.Email, "Confirm your account",
+                    $"Подтвердите регистрацию, перейдя по ссылке: <a href='{callbackUrl}'>link</a>");
+
+                await dbContext.Set<User>().AddAsync(user);
+                await dbContext.SaveChangesAsync();
+                // установка куки
+                await signInManager.SignInAsync(applicationUser, false);
                 return RedirectToAction("Get", "Meetup");
             }
+            await userManager.DeleteAsync(applicationUser);
             return Unauthorized();
         }
 
         [HttpPost("Login")]
         public async Task<IActionResult> LoginAsync(LoginViewModel model)
         {
-            return await userService.LoginAsync(model);
+            var user = await userManager.FindByNameAsync(model.Email);
+            if (user != null)
+            {
+                // проверяем, подтвержден ли email
+                if (!await userManager.IsEmailConfirmedAsync(user))
+                {
+                    ModelState.AddModelError(string.Empty, "Вы не подтвердили свой email");
+                    return Unauthorized(model);
+                }
+            }
+
+            var result = await signInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, false);
+            if (result.Succeeded)
+            {
+                await signInManager.SignInAsync(user, false);
+                return RedirectToAction("Get", "Meetup");
+            }
+            else
+            {
+                return Unauthorized();
+            }
         }
 
         [HttpPost("Logout")]
         public async Task<IActionResult> Logout()
         {
-            return await userService.Logout();
+            // удаляем аутентификационные куки
+            await signInManager.SignOutAsync();
+            return RedirectToAction("Get", "Meetup");
         }
 
-        [Authorize(Roles = "admin")]
-        [HttpGet("admin/Users")]
-        public IActionResult GetAllUser()
+        [HttpGet("ConfirmEmail")]
+        [AllowAnonymous]
+        public async Task<IActionResult> ConfirmEmail(string userId, string code)
         {
-            var result = adminService.GetAllUsers();
-            return Ok(result);
-        }
-
-        [Authorize(Roles = "admin")]
-        [HttpGet("admin/User/{id}")]
-        public IActionResult GetUserById(int id)
-        {
-            var result = adminService.GetUserById(id);
-            return Ok(result);
+            if (userId == null || code == null)
+            {
+                return BadRequest();
+            }
+            var user = await userManager.FindByIdAsync(userId);
+            if (user == null)
+            {
+                return BadRequest();
+            }
+            var result = await userManager.ConfirmEmailAsync(user, code);
+            if (result.Succeeded)
+            {
+                user.EmailConfirmed = true;
+                await userManager.UpdateAsync(user);
+                return RedirectToAction("Get", "Meetup");
+            }
+            else
+                return BadRequest();
         }
     }
 }
---
+++ Meetup/Services/EmailSender.cs
@@ -0,0 +1,51 @@
+﻿using MailKit.Net.Smtp;
+using Meetup.Services.Interfaces;
+using Microsoft.Extensions.Options;
+using MimeKit;
+using System.Threading.Tasks;
+
+namespace Meetup.Services
+{
+    public class EmailSender : IEmailSender
+    {
+        EmailOptions options;
+        public EmailSender(IOptions<EmailOptions> options)
+        {
+            this.options = options.Value;
+        }
+
+        public async Task SendEmailAsync(string email, string subject, string htmlMessage)
+        {
+            var mimeMessage = MimeMessage(email, subject);
+
+            mimeMessage.Body = new TextPart(MimeKit.Text.TextFormat.Html)
+            { Text = htmlMessage };
+
+            await Send(mimeMessage);
+        }
+
+        private MimeMessage MimeMessage(string email, string subject)
+        {
+            var mimeMessage = new MimeMessage();
+            mimeMessage.From.Add(MailboxAddress.Parse(options.Sender));
+            mimeMessage.To.Add(MailboxAddress.Parse(email));
+            mimeMessage.Subject = subject;
+
+
+            return mimeMessage;
+        }
+
+        private async Task Send(MimeMessage mimeMessage)
+        {
+            using SmtpClient smtpClient = new SmtpClient();
+
+            smtpClient.ServerCertificateValidationCallback += (s, c, h, e) => true;
+            await smtpClient.ConnectAsync(options.SmtpServer, options.Port, true);
+            await smtpClient.AuthenticateAsync(options.UserName, options.Password);
+            await smtpClient.SendAsync(mimeMessage);
+            await smtpClient.DisconnectAsync(true);
+        }
+    }
+
+
+}
